977ceff71b11dc662212d8ffbb2e8f6a40c47b6d
milton <miltoncaraball@yahoo.com.ar>
2022-06-08 09:08:33 -0300

    react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+//@ts-check
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Martin esta es mi primera entrega en React.</h1>
+      <p onClick={() => alert('Hola Mundo')} style={{color:"red"}}>Soy Milton Caraball Teodoracopulos alumno de la comision 37750</p>
     </div>
   );
-}
+  // let myArray = [
+  //               {id: 100, nombre:"milton", apellido:"caraball"},
+  //               {id: 101, nombre: "guille", apellido:"forma"}
+  //             ];
+
+  //        // console.log(...myArray); 
+  //        //clono el array y le agrego otros objetos     
+  //     const array2=[...myArray, {id: 100, nombre:"milton", apellido:"caraball"}];
+  //     console.log(array2);
+
+  //     //desestructuracion
+  //     //saco el nombre del objeto
+  //     const {nombre} = {id:182, nombre:"juan", apellido:"tres"};
+  //     //console.log("tu nombre es "+ nombre);
+
+      //-- MAP: TENGO ESTO
+      // let myArray2 = [
+      //           {id: 100, nombre:"milton", apellido:"caraball"},
+      //           {id: 101, nombre: "guille", apellido:"forma"}
+      //         ];
+              //quiero Mostrar:
+          //  const resultado = myArray2.map(item)=> <div>{item.nombre}</div>
+          //     return <div>{resultado}</div>
+          }
 
 export default App;
